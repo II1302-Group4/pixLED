120e42c883e8d94048cf353ba0f4aa4940d96270
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-04-18 16:33:30 +0200

    feat: add feature to update the model and database

---
+++ src/firebaseModel.jsx
@@ -0,0 +1,43 @@
+import * as firebase from "firebase/app";
+import { getDatabase, get, child, ref, set, onValue } from "firebase/database";
+
+import firebaseConfig from "./firebaseConfig";
+
+import PixLEDModel from "./PixLEDModel";
+
+firebase.initializeApp(firebaseConfig);
+
+const db = getDatabase();
+
+function updateFirebaseFromModel(model) {
+  model.addObserver(async function (payload) {
+    if (payload) {
+      if (payload.hasOwnProperty("ledColor")) {
+        set(ref(db, "ledMatrix/" + payload.ledNumber), payload.ledColor);
+      }
+    }
+  });
+}
+
+function updateModelFromFirebase(model) {
+  const ledMatrixRef = ref(db, "ledMatrix");
+  onValue(ledMatrixRef, (firebaseData) => {
+    model.setGridArray(Object.values(firebaseData.val()));
+  });
+}
+
+async function firebaseModelPromise() {
+  const ledMatrixData = await get(child(ref(db), "ledMatrix"));
+  if (ledMatrixData.exists()) {
+    const ledMatrixArray = Object.values(ledMatrixData.val());
+    return new PixLEDModel(ledMatrixArray);
+  }
+
+  return new PixLEDModel();
+}
+
+export {
+  firebaseModelPromise,
+  updateFirebaseFromModel,
+  updateModelFromFirebase,
+};

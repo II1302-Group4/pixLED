718419776632d5dbf758cef2a42c847e9f06b9c8
Youssef <saw123789@gmail.com>
2023-04-18 11:53:39 +0200

    fix;button disappears after click

--- src/views/matrixGridView.jsx
+++ src/views/matrixGridView.jsx
@@ -3,12 +3,17 @@ import "../App.css";
 
 export default function matrixGridView(props) {
   function makeMatrix() {
+    hideButton();
     props.makeMatrix(64, 64);
+  }
+  function hideButton() {
+    document.getElementById("showMatrixButton").style.display="none";
+    
   }
 
   return (
     <div className="container">
-      <div onClick={makeMatrix}>Click on this to create the matrix</div>
+      <button id="showMatrixButton" onClick={makeMatrix}>click me!</button>
     </div>
   );
 }

5dedf7e42fc0410cc1ebc81df600878c099e6b8c
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-04-26 08:13:11 +0200

    chore: remove console statement

--- src/presenters/matrixGridPresenter.jsx
+++ src/presenters/matrixGridPresenter.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MatrixGridView from "../views/matrixGridView";
 
-
 export default function MatrixGrid(props) {
   const [matrixGrid, setMatrixGrid] = React.useState(props.model.gridArray);
   const [pickedColor, setPickedColor] = React.useState(
@@ -11,30 +10,26 @@ export default function MatrixGrid(props) {
   const initialTimer = props.model.timer;
   const [timer, setTimer] = React.useState(initialTimer);
   const [sumbit, setSumbit] = React.useState(false);
-  const timeoutId = React.useRef(null);  
-  const [timeout, ] = React.useState(15);
-
+  const timeoutId = React.useRef(null);
+  const [timeout] = React.useState(15);
 
   const countTimer = React.useCallback(() => {
-        if (timer <= 0) {
-            setTimer(timeout);
-            setSumbit(false);
-        } else {
-          console.log("timer")
-          if(sumbit) {
-            setTimer(timer - 1);
-            props.model.updateTimer(timer);
-          }
-    }}, [timer, sumbit]);
+    if (timer <= 0) {
+      setTimer(timeout);
+      setSumbit(false);
+    } else {
+      if (sumbit) {
+        setTimer(timer - 1);
+        props.model.updateTimer(timer);
+      }
+    }
+  }, [timer, sumbit]);
 
-    
-
-
-    React.useEffect(() => {
-        timeoutId.current = window.setTimeout(countTimer, 1000);
-        // cleanup function
-        return () => window.clearTimeout(timeoutId.current);
-    }, [timer, countTimer]);
+  React.useEffect(() => {
+    timeoutId.current = window.setTimeout(countTimer, 1000);
+    // cleanup function
+    return () => window.clearTimeout(timeoutId.current);
+  }, [timer, countTimer]);
 
   React.useEffect(wasCreatedACB, []);
 
@@ -56,7 +51,7 @@ export default function MatrixGrid(props) {
     props.model.updateColorInDatabase(color, ledNumber);
     setSumbit(true);
   }
-  
+
   /**
    * Asks model to select a pixel
    * @param {int} ledNumber LED's index in the matrix array
@@ -67,16 +62,16 @@ export default function MatrixGrid(props) {
 
   return (
     <>
-    <div align="center">Timer :{timer}</div>
-    <MatrixGridView
-      matrixGrid={matrixGrid}
-      updateColor={updateColor}
-      selectLED={selectLED}
-      chosenLED={chosenLed}
-      chosenColor={pickedColor}
-      timer={timer}
-      timeout={timeout}
-    />
+      <div align="center">Timer :{timer}</div>
+      <MatrixGridView
+        matrixGrid={matrixGrid}
+        updateColor={updateColor}
+        selectLED={selectLED}
+        chosenLED={chosenLed}
+        chosenColor={pickedColor}
+        timer={timer}
+        timeout={timeout}
+      />
     </>
   );
 }

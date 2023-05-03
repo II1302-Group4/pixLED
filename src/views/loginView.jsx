a38d158ad304bad494354fe0b6b68d23f25b1c63
Jesper Henriksson <jesper.victorsson@gmail.com>
2023-05-03 09:58:40 +0200

    feat. dropdownbar
    design

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../firebaseModel";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import googleLogo from "../assets/google-logo.png";
 
-function loginView(props) {
-  const provider = new GoogleAuthProvider();
+function LoginView(props) {
+    const provider = new GoogleAuthProvider();
 
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, provider);
-    } catch (error) {
-      console.error("Error signing in with Google", error);
-    }
-  };
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            console.error("Error signing in with Google", error);
+        }
+    };
 
-  const signOutWithGoogle = async () => {
-    try {
-      await signOut(auth);
-      props.model.setCurrentUser(null);
-    } catch (error) {
-      console.error("Error signing out with Google", error);
-    }
-  };
-  return (
-    <div>
-      {props.isLoggedIn ? (
-        <button className="txt-btn" onClick={signOutWithGoogle}>
-          Sign out
-        </button>
-      ) : (
-        <button className="txt-btn" onClick={signInWithGoogle}>
-          <img src={googleLogo} id="google-logo"/>
-          Login
-        </button>
-      )}
-    </div>
-  );
+    const signOutWithGoogle = async () => {
+        try {
+            await signOut(auth);
+            props.model.setCurrentUser(null);
+        } catch (error) {
+            console.error("Error signing out with Google", error);
+        }
+    };
+
+    const [showOptions, setShowOptions] = useState(false);
+
+    //callback if "options" pressed, then display the options. the dropdownbar function
+    const handleOptionsClick = () => {
+        setShowOptions(!showOptions);
+    };
+    return (
+        <div>
+            {props.isLoggedIn ? (
+                <>
+                    <button className="txt-btn" onClick={handleOptionsClick}>
+                        Options
+                    </button>
+                    {showOptions && (
+                        <div className="dropdown-menu">
+                            <ul>
+                                <button className="txt-btn"> Profile </button>
+                                <button className="txt-btn"> Groups </button>
+                                <button className="txt-btn">
+                                    History Of Changes
+                                </button>
+                                <button className="txt-btn">Artist Mode</button>
+                                <button
+                                    className="txt-btn"
+                                    onClick={signOutWithGoogle}
+                                >
+                                    Sign out
+                                </button>
+                            </ul>
+                        </div>
+                    )}
+                </>
+            ) : (
+                <button className="txt-btn" onClick={signInWithGoogle}>
+                    <img src={googleLogo} id="google-logo" />
+                    Login
+                </button>
+            )}
+        </div>
+    );
 }
 
-export default loginView;
+export default LoginView;

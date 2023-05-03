b1dec95e055d2c15f87bf6c4b1ac0f55259fe90b
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-05-03 15:21:58 +0200

    feat: user must be logged in to accept invitation
    Co-Authored-By: m7tar <91196060+m7tar@users.noreply.github.com>

--- src/views/joinGroupView.jsx
+++ src/views/joinGroupView.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import JoinGroupPresenter from "../presenters/joinGroupPresenter";
 
 function JoinGroupView(props) {
-  return (
-    <div>
-      <h1>You have been invited to join Group</h1>
-      <button>Accept invitation</button>
-      <button>Decline invitation</button>
-    </div>
-  );
+  if (!props.model.currentUser) {
+    return <div>Please login and try again</div>;
+  } else {
+    return (
+      <div>
+        <h1>You have been invited to join Group {props.groupName}</h1>
+        <button onClick={() => props.acceptInvitation()}>
+          Accept invitation
+        </button>
+        <button>Decline invitation</button>
+      </div>
+    );
+  }
 }
 
 export default JoinGroupView;

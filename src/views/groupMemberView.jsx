3e28105fba9d577ad937fef3cce701cc7dbf2b43
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-05-04 10:08:38 +0200

    feat: make it possible to see members on main page

---
+++ src/views/groupMemberView.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function GroupMemberView(props) {
+  function Member(member, index) {
+    return (
+      <li key={index}>
+        {member.name}{" "}
+        <button style={{ backgroundColor: `${member.color}` }}>hej</button>
+      </li>
+    );
+  }
+  return (
+    <div>
+      <ul>{props.members.map(Member)}</ul>
+    </div>
+  );
+}
+
+export default GroupMemberView;
--- src/views/mainView.jsx
+++ src/views/mainView.jsx
@@ -1,6 +1,7 @@
 import MatrixGridPresenter from "../presenters/matrixGridPresenter";
 import ColorPalettePresenter from "../presenters/colorPalettePresenter";
 import { useNavigate } from "react-router-dom";
+import GroupMembers from "../presenters/groupMembersPresenter";
 function MainView(props) {
   const navigate = useNavigate();
   function createGroup() {
@@ -15,6 +16,7 @@ function MainView(props) {
       <div className="colorPalette">
         <ColorPalettePresenter model={props.model} />
       </div>
+      <GroupMembers model={props.model} />
       {/* <button onClick={createGroup}>Create a group</button> */}
     </div>
   );
